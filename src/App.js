d4a3f7e654cb0ac8a7384e5a7e1160751580c136
sejal966 <126438376+sejal966@users.noreply.github.com>
2023-08-18 20:04:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Item from './components/Item';
+
+function App() {
+  return (
+    <div>
+    <Item></Item>
+    <div className="App">
+      Hello Jeeeee
+    </div>
+    </div>
+
+  );
+}
+
+export default App;
